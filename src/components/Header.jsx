d3d9733b2303ce04076f6019ea334d39c462484c
Debo Gbadebo <111382307+Jlbspyder@users.noreply.github.com>
2023-09-13 21:51:46 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import { BsMoon, BsMoonFill } from "react-icons/bs";
+import { useGetUserInfo } from "../hooks/useGetUserInfo";
 
 export const Header = ({ mode, lightmode }) => {
+  const { name } = useGetUserInfo();
+
   return (
     <div className="mode" onClick={mode}>
-      {lightmode ? <BsMoon className="moon" /> : <BsMoonFill className="fill" />}
+      {lightmode ? (
+        <BsMoon className="moon" />
+      ) : (
+        <BsMoonFill className="fill" />
+      )}
+      <h6>Hello, {name}</h6>
     </div>
   );
 };
