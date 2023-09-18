b3fdb63c28dfeb05a66d2cb9663d4df545fb4e4e
Debo Gbadebo <111382307+Jlbspyder@users.noreply.github.com>
2023-09-18 22:31:28 +0100

    Update and rename useGetActivity.jsx to useGetActivities.jsx

---
+++ src/hooks/useGetActivities.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { query, collection, where, onSnapshot, orderBy } from 'firebase/firestore'
+import { useGetUserInfo } from "../hooks/useGetUserInfo";
+import { db } from "../pages/firebase-config";
+
+export const useGetActivities = () => {
+    const [activities, setActivities] = useState([])
+    
+    const activityRef = collection(db, "activities");
+    const {userID} = useGetUserInfo()
+  
+    const getActivity = async () => {
+      let cleanup
+      try {
+        const queryActivity = query(activityRef, where('userID', "==", userID),
+        orderBy('createdAt'))
+  
+        cleanup = onSnapshot(queryActivity, (snapshot) => {
+          let snaps = []
+            snapshot.forEach((snap) => {
+            const data = snap.data()
+            const id = snap.id
+  
+            snaps.push({...data, id})
+          })
+          setActivities(snaps)
+        })
+      } catch (error) {
+        console.error(error)
+      }
+      return () => cleanup()
+    }
+  
+    useEffect(() => {
+      getActivity()
+    }, [])
+    
+    return { activities }
+  }
